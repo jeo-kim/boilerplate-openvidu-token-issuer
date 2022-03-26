b41ed0494ed1954bd7cee10c0b20078ea46645d6
최호준 <67067346+wns6743@users.noreply.github.com>
2022-03-26 14:41:15 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
+# sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
 
 REPOSITORY=/home/ubuntu/app
 
