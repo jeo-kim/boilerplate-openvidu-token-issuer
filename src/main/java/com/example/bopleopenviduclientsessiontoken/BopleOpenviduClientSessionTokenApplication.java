860b3a9daf908945844c17c23aa7fea8d833b35f
jeonghyeon <skjh0807s@snu.ac.kr>
2022-04-03 03:23:02 +0900

    [try] Cors bean으로 시도.

--- src/main/java/com/example/bopleopenviduclientsessiontoken/BopleOpenviduClientSessionTokenApplication.java
+++ src/main/java/com/example/bopleopenviduclientsessiontoken/BopleOpenviduClientSessionTokenApplication.java
@@ -2,12 +2,26 @@ package com.example.bopleopenviduclientsessiontoken;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BopleOpenviduClientSessionTokenApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BopleOpenviduClientSessionTokenApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000", "https://localhost:3000",
+                        "https://127.0.0.1:3000", "http://127.0.0.1:3000", "https://boiler-plate.org").allowedOriginPatterns("*");
+            }
+        };
     }
 
 }
